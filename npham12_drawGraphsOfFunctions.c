9d31f483312d9cc270772286825155c51f5d6579
Billy <40702630+NPHAM12@users.noreply.github.com>
2019-02-22 14:32:38 -0800

    Update npham12_drawGraphsOfFunctions.c

--- npham12_drawGraphsOfFunctions.c
+++ npham12_drawGraphsOfFunctions.c
@@ -1,8 +1,3 @@
-/* Draw graphs of functions
- Billy Pham*/
-
-/*This program will draw (rather rough) graphs of functions. The function being graphed and the region of the graph to be displayed will be hardcoded into the program, but in such a way that the program can easily be modified and recompiled to graph different functions and display different sized regions of the graph
- */
 #include<stdio.h>
 #include<string.h>
 #include<math.h>
